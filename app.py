fab1a091a5ad97230a37f06d387a939ff6a624bb
Sergeeva Julia <32176800+Emidiant@users.noreply.github.com>
2020-12-17 17:49:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,40 @@ import traceback
 import json
 from model.model import ModelLoader
 from data.db import load_by_ids
+import pandas as pd
+from flasgger import Swagger
+from flasgger.utils import swag_from
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
+app.config["SWAGGER"] = {"title": "Swagger-UI", "uiversion": 2}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ml_service:ml_pass@postgres:5432/ml_service_db"
+db = SQLAlchemy(app)
+df_db = pd.read_csv('data/alcohol_15000.csv')
+df_db.columns = [c.lower() for c in df_db.columns]
+df_db.to_sql("wines", app.config['SQLALCHEMY_DATABASE_URI'])
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": "predict",
+            "route": "/ml-description-based-recommendation-service",
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True
+        }
+    ],
+    "static_url_path": "/flagger_static",
+    "swagger_ui": True,
+    "specs_route": "/swagger/"
+}
+
+swagger = Swagger(app, config=swagger_config)
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['GET'])
+@swag_from("swagger/swagger_config_predict.yml")
 def predict():
     if request.method == 'GET':
         try:
@@ -28,11 +57,9 @@ def predict():
                 "trace": traceback.format_exc()
             })
 
-    if request.method == 'POST':
-        return "Page post"
 
-
-@app.route('/train', methods=['POST'])
+@app.route('/retrain', methods=['POST'])
+@swag_from("swagger/swagger_config_retrain.yml")
 def train():
     if request.method == 'POST':
         try:
