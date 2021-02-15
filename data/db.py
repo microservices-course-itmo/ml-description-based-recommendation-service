c2194ecc05636e3c9c664d1b203a37ab80ddfaef
Sergeeva Julia <32176800+Emidiant@users.noreply.github.com>
2021-02-15 18:48:12 +0300

    Update db.py

--- data/db.py
+++ data/db.py
@@ -6,20 +6,20 @@ from sqlalchemy import create_engine
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://os.environ.get('S_POSTGRES_USER'):os.environ.get('S_POSTGRES_PASSWORD')@os.environ.get('S_POSTGRES_HOST'):5432/os.environ.get('S_POSTGRES_DB')"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://os.environ.get('S_POSTGRES_USER'):os.environ.get('S_POSTGRES_PASSWORD')@os.environ.get('S_POSTGRES_HOST'):5432/os.environ.get('S_POSTGRES_DB')"
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# db = SQLAlchemy(app)
+engine = create_engine('postgresql://ml_service:ml_pass@ml_service_db:5432/ml_service_db', echo=True)
+migrate = Migrate(app, engine)
 
-engine = create_engine('postgresql://ml_service:ml_pass@db:5432/ml_service_db', echo=True)
 
 def load_all():
-    df = pd.read_sql(f'SELECT * FROM wines', app.config['SQLALCHEMY_DATABASE_URI'])
+    df = pd.read_sql(f'SELECT * FROM wines', engine)
     df['description'] = df['aroma'] + ' ' + df['taste'] + ' ' + df['description']
     return df
 
 
 def load_by_ids(ids):
     indices = tuple(ids)
-    alcohol = pd.read_sql(f'SELECT id, name, type, crop_year, manufacturer, brand, volume, country, region, color, grape, sugar FROM wines WHERE id IN {indices}', app.config['SQLALCHEMY_DATABASE_URI'])
+    alcohol = pd.read_sql(f'SELECT id, name, type, crop_year, manufacturer, brand, volume, country, region, color, grape, sugar FROM wines WHERE id IN {indices}', engine)
     return alcohol
