edeffc03116d33fd94daead3de16c2f5f0cca20a
pchizhov <chizhov.pasha@yandex.ru>
2021-03-16 18:49:24 +0300

    selecting order fixed

--- data/db.py
+++ data/db.py
@@ -21,16 +21,14 @@ def drop_table(table_name):
 
 def load_catalogue():
     catalogue = create_engine("postgresql://catalog_service_reader:readonly@77.234.215.138:18095/catalog_service_db")
-    df = pd.read_sql(f"select wine_id, {fields} from wine_position limit 1000", catalogue)
+    df = pd.read_sql(f"select wine_id, {fields} from wine_position order by wine_id limit 1000", catalogue)
     print(f'Loaded {df.shape[0]} records from catalogue')
     df.to_sql("wines", engine)
 
 
 def load_all():
-    df = pd.read_sql(f'SELECT wine_id, {fields} FROM wines', engine)
+    df = pd.read_sql(f'SELECT wine_id, {fields} FROM wines order by wine_id', engine)
     # df['description'] = df[fields.split(', ')].agg(' '.join, axis=1)
-
-    # df['description'] = ' '.join([df[field] for field in fields.split(', ')])
     return df
 
 
